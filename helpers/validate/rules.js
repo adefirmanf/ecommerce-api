7820270192abc644f1ab2d8041790eeb622fa0f5
adefirmanf <syskeys32@gmail.com>
2020-04-04 22:56:18 +0700

    add array rule

--- helpers/validate/rules.js
+++ helpers/validate/rules.js
@@ -2,6 +2,7 @@ module.exports = {
   required: (rules = 'string') => `required|${rules}`,
   string: () => `string`,
   email: () => `email|`,
+  array: () => `array`,
   min: (min) => `min:${min}|`,
   max: (max) => `max:${max}|`,
   url: () => `url|`
