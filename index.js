857ae804a6145432697e28c8e6b4aa654a1a3093
Ade Firman Fauzi <syskeys32@gmail.com>
2020-06-17 00:05:42 +0700

    enable cors

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const app = require('./bin')
-const authRoutes = require('./routes/authRoute')
-const productRoutes = require('./routes/productRoute')
+const app = require("./bin");
+const authRoutes = require("./routes/authRoute");
+const productRoutes = require("./routes/productRoute");
 
-app.use("/auth", authRoutes)
-app.use("/products", productRoutes)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
 
 app.use("*", (req, res, next) => {
-  res.sendStatus(404)
-})
+  res.sendStatus(404);
+});
