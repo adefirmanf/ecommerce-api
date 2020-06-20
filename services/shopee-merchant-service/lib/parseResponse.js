68af4ab0a675c9df47038fa438c532dc38d44d6c
Ade Firman Fauzi <syskeys32@gmail.com>
2020-06-20 16:49:22 +0700

    fix image shopee

--- services/shopee-merchant-service/lib/parseResponse.js
+++ services/shopee-merchant-service/lib/parseResponse.js
@@ -1,61 +1,64 @@
-const merchantName = "shopee"
+const merchantName = "shopee";
 
 module.exports = (data) => {
   if (data.items.length < 1) {
-    return [{
-      name: "",
-      href: "",
-      img: [],
-      price: 0,
-      priceDetails: {
-        priceDisplay: 0,
-        discount: 0,
-        strikeDisplay: 0
+    return [
+      {
+        name: "",
+        href: "",
+        img: [],
+        price: 0,
+        priceDetails: {
+          priceDisplay: 0,
+          discount: 0,
+          strikeDisplay: 0,
+        },
+        attributes: [],
+        review: {
+          rating: 0,
+          count: 0,
+        },
+        brand: "",
+        merchant: {
+          name: merchantName,
+          productId: "",
+          sku: "",
+          shopId: "",
+        },
       },
-      attributes: [],
-      review: {
-        rating: 0,
-        count: 0
-      },
-      brand: '',
-      merchant: {
-        name: merchantName,
-        productId: "",
-        sku: "",
-        shopId: ""
-      },
-    }]
+    ];
   }
   return data.items.map((n) => {
-    const priceFormat = (v) => parseInt(v.toString().slice(0, v.toString().length - 5))
+    const priceFormat = (v) =>
+      parseInt(v.toString().slice(0, v.toString().length - 5));
     return {
       name: n.name,
       href: "",
-      img: n.images.map(img => 'https://cf.shopee.co.id/file/' + img),
+      img: n.images.map((img) => "https://cf.shopee.co.id/file/" + img),
       category: n.catid,
       price: priceFormat(n.price),
       priceDetails: {
         priceDisplay: priceFormat(n.price),
         discount: priceFormat(n.show_discount),
-        strikeDisplay: priceFormat(n.price_max) || 0
+        strikeDisplay: priceFormat(n.price_max) || 0,
       },
       attributes: n.tier_variations.map((j) => {
         return {
           optionListingType: j.name,
-          values: j.options
-        }
+          values: j.options,
+        };
       }),
       review: {
         rating: n.item_rating.rating_star,
-        count: n.item_rating.rating_count
+        count: n.item_rating.rating_count,
       },
       brand: n.brand,
       merchant: {
         name: merchantName,
         productId: n.itemid,
-        sku: n.itemid,
-        shopId: n.shopid
-      }
-    }
-  })
-}
+        sku: n.shopid,
+        shopId: n.shopid,
+      },
+    };
+  });
+};
